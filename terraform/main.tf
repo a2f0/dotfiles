78052c2257a69ae1d2b6391a0c932d20cbccf4ef
Dan Sullivan <dansullivan@gmail.com>
2021-05-15 14:27:14 -0400

    Feature/post to slack (#4)
    * post to slack when errors occur

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "github" {
+  token = var.github_access_token
+  owner = var.github_owner
+}
+
+resource "github_actions_secret" "slack_webhook_url" {
+  repository       =  var.github_repository
+  secret_name      = "SLACK_WEBHOOK_URL"
+  plaintext_value  = var.slack_webhook_url
+}
