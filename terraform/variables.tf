34270fba48e43fb356c8a76520e0d3a1397c8e74
Dan Sullivan <dansullivan@gmail.com>
2021-05-15 14:08:53 -0400

    post to slack

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "github_owner" {
+    type = string
+}
+
+variable "github_repository" {
+    type = string
+}
+
+variable "github_access_token" {
+    type = string
+}
+
+variable "slack_webhook_url" {
+    type = string
+}
