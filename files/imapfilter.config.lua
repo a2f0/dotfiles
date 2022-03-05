21043f098aec14efd5a4767154035b46cab68682
Dan Sullivan <dansullivan@gmail.com>
2022-03-05 04:15:31 -0500

    abstract password to environment

--- files/imapfilter.config.lua
+++ files/imapfilter.config.lua
@@ -3,8 +3,19 @@ options.create = true
 options.subscribe = true
 options.expunge = true
 
-account1 = IMAP {
+gmail = IMAP {
     server = "imap.gmail.com",
     username = "dansullivan@gmail.com",
+    password = os.getenv("GMAIL_APP_SPECIFIC_PASSWORD"),
     ssl = "tls1"
 }
+
+senders = {
+    "spammer@spammerdomain.com", "youtube.com", "Mailer Delivery System"
+}
+
+for sender = 1, #senders do
+    print("Removing messages from " .. senders[sender])
+    -- messages = gmail["[Gmail]/All Mail"]:contain_from(senders[sender])
+    -- messages:move_messages(account1["[Gmail]/Trash"])
+end
