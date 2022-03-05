f72479a98834f2b1b8e1b493c85171f216521b65
a2f0 <dansullivan@gmail.com>
2022-03-05 03:43:57 -0600

    Imapfilter (#47)
    * install /configure imapfilter

---
+++ files/imapfilter.config.lua
@@ -0,0 +1,30 @@
+options.timeout = 1200
+options.create = true
+options.subscribe = true
+options.expunge = true
+
+gmail = IMAP {
+    server = "imap.gmail.com",
+    username = "dansullivan@gmail.com",
+    password = os.getenv("GMAIL_APP_SPECIFIC_PASSWORD"),
+    ssl = "tls1"
+}
+
+mailboxes, folders = gmail:list_all()
+print("=== mailboxes")
+for _, m in ipairs(mailboxes) do print(m) end
+print("=== folders")
+for _, f in ipairs(folders) do print(f) end
+
+-- Cleanup Spam Folder
+senders = {
+    "americastestkitchen.com",
+    "marcusmillichap.com"
+}
+
+for sender = 1, #senders do
+    print("Removing messages from " .. senders[sender])
+    messages = gmail["Junk"]:contain_from(senders[sender])
+
+    -- messages:move_messages(account1["[Gmail]/Trash"])
+end
