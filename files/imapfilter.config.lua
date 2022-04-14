01927c898f6b780ab659067e38f74ea3690987d1
Dan Sullivan <dansullivan@gmail.com>
2022-04-14 07:42:16 -0400

    chore: tune mail filters

--- files/imapfilter.config.lua
+++ files/imapfilter.config.lua
@@ -16,6 +16,21 @@ for _, m in ipairs(mailboxes) do print(m) end
 print("=== folders")
 for _, f in ipairs(folders) do print(f) end
 
+-- Get all
+all_messages = gmail["INBOX"]:select_all()
+
+-- Jira
+messages = gmail["INBOX"]:contain_subject('[JIRA]'):contain_from('atlassian.net')
+messages:move_messages(gmail["Keep/Jira"])
+messages = gmail["Keep/Jira"]:is_older(30)
+messages:move_messages(gmail['[Gmail]/Trash'])
+
+-- Confluence
+messages = gmail["INBOX"]:contain_subject('[Confluence]'):contain_from('atlassian.net')
+messages:move_messages(gmail["Keep/Confluence"])
+messages = gmail["Keep/Confluence"]:is_older(30)
+messages:move_messages(gmail['[Gmail]/Trash'])
+
 -- Cleanup GitHub
 messages = gmail["INBOX"]:contain_from("notifications@github.com")
 messages:move_messages(gmail["Keep/GitHub"])
