9723cfc50afcad03d8fcc37e8b506731d45045e6
Dan Sullivan <dansullivan@gmail.com>
2022-11-19 17:53:16 +0000

    config: add firefox spawn to xmonad config

--- files/xmonad.hs
+++ files/xmonad.hs
@@ -4,4 +4,9 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
 main :: IO ()
-main = xmonad def
+main = xmonad $ def
+    { modMask = mod4Mask -- remap modifier key from alt to super.
+    }
+  `additionalKeysP`
+    [ ("M-f", spawn "firefox")
+    ]
