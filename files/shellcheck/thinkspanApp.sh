9753089c91b6bd26b21fbf19bc278d62e4e4edd8
Dan Sullivan <dansullivan@gmail.com>
2023-07-14 08:55:48 -0400

    fix: load project

--- files/shellcheck/thinkspanApp.sh
+++ files/shellcheck/thinkspanApp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -i
 cd ~/github-thinkspan/ThinkspanApp/android || exit
 nvm use
-/opt/android-studio-2021.3.1/android-studio/bin/studio.sh
+/opt/android-studio-2021.3.1/android-studio/bin/studio.sh .
